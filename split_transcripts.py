44f52b4c64e0b49d397cdce102aa9f69223bd312
Skordio <skordion@hotmail.com>
2024-04-15 14:32:36 -0400

    make sure script removes files that are already there if reprocessing the transcript

--- split_transcripts.py
+++ split_transcripts.py
@@ -4,9 +4,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Split the transcripts into separate files')
     parser.add_argument('-t', '--transcripts', type=str, default='transcripts.txt', help='Path to the transcripts file')
     parser.add_argument('-o', '--output', type=str, default='output', help='Path to the output directory')
+    parser.add_argument('--overwrite', action='store_true', default=True, help='Overwrite the existing transcription files')
     return parser.parse_args()
 
-def process_file(transcripts_file, transcripts_dir):
+def process_file(transcripts_file, transcripts_dir, overwrite=True):
     with open(transcripts_file, encoding='utf8') as trans_file:
         lines = trans_file.readlines()
         
@@ -26,19 +27,22 @@ def process_file(transcripts_file, transcripts_dir):
             clip_id = id_section[2]
             
             speaker_and_section = f'{speaker_id}-{section_id}'
+                    
+            dir_path = f'{transcripts_dir}\{speaker_id}\{section_id}'
+            file_path = f'{dir_path}\{speaker_id}-{section_id}.trans.txt'
             
             if speaker_id not in speakers_processed:
                 speakers_processed.append(speaker_id)
                 print(f'Processing speaker {speaker_id}')
                 
-                if speaker_and_section not in sections_processed:
-                    sections_processed.append(speaker_and_section)
-                    
-            dir_path = f'{transcripts_dir}\{speaker_id}\{section_id}'
-            file_path = f'{dir_path}\{speaker_id}-{section_id}.trans.txt'
+            if speaker_and_section not in sections_processed:
+                sections_processed.append(speaker_and_section)
+        
+                if not os.path.exists(dir_path):
+                    os.makedirs(dir_path)
             
-            if not os.path.exists(dir_path):
-                os.makedirs(dir_path)
+                if overwrite and os.path.exists(file_path):
+                    os.remove(file_path)
                 
             with open(file_path, 'a', encoding='utf8') as new_file:
                 new_file.write(f'{speaker_id}-{section_id}-{clip_id} {" ".join(words).upper()}\n')
@@ -47,6 +51,6 @@ if __name__ == '__main__':
     args = parse_args()
     
     transcripts_file = os.path.join(os.getcwd(), args.transcripts)
-    transcripts_dir = os.path.dirname(transcripts_file)
+    transcripts_dir = os.path.join(os.path.dirname(transcripts_file), args.output)
     
-    process_file(transcripts_file, transcripts_dir)
+    process_file(transcripts_file, transcripts_dir, args.overwrite)
